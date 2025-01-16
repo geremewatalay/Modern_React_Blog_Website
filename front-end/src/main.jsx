f6c9a5182bd9f042e7cefec7a38ede726ae375b1
geremewatalay <geremewataly@gmail.com>
2025-01-16 05:44:37 -0800

    navbar commit

---
+++ front-end/src/main.jsx
@@ -0,0 +1,49 @@
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home.jsx';
+import Blogs from './pages/Blogs.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Services from './pages/Services.jsx';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App/>,
+    children:[
+      {
+        path:'/',
+        element:<Home/>
+      },
+      {
+        path:'/blogs',
+        element:<Blogs/>
+      },
+      {
+        path:'/about',
+        element:<About/>
+      },
+      {
+        path:'/contact',
+        element:<Contact/>
+      },
+      {
+        path:'/services',
+        element:<Services/>
+      }
+
+    ]
+  },
+])
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
